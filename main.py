2626844d96c0848afd585506e9068030a0989699
Sandali426 <it22088314@my.sliit.lk>
2024-12-13 11:22:45 +0530

    change calculator to calculate infinite times

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
-#python calculator
+def calculator():
+    while True:
+        operator = input("Enter an operator (+, -, *, /) or 'exit' to quit: ")
 
-operator = input("Enter an operator: (+ - * /):")
-num1 = float (input("Enter the 1st Number : "))
-num2 = float(input("Enter the 2nd Number : "))
+        if operator.lower() == "exit":
+            print("Exiting the calculator. Goodbye!")
+            break
 
-if operator == "+":
-    result = num1 + num2
-    print(round(result,3))
-elif operator == "-":
-    result = num1 + num2
-    print(round(result,3))
-elif operator == "*":
-    result = num1 * num2
-    print(round(result,3))
-elif operator == "/":
-    result = num1 / num2
-    print(round(result,3))
-else:
-    print(f"{operator} is not a valid operator")
+        try:
+            num1 = float(input("Enter the 1st Number: "))
+            num2 = float(input("Enter the 2nd Number: "))
+
+            if operator == "+":
+                result = num1 + num2
+                print(round(result, 3))
+            elif operator == "-":
+                result = num1 - num2
+                print(round(result, 3))
+            elif operator == "*":
+                result = num1 * num2
+                print(round(result, 3))
+            elif operator == "/":
+                if num2 == 0:
+                    print("Error: Division by zero is not allowed.")
+                else:
+                    result = num1 / num2
+                    print(round(result, 3))
+            else:
+                print(f"{operator} is not a valid operator")
+        except ValueError:
+            print("Invalid input. Please enter numeric values.")
+
+if __name__ == "__main__":
+    calculator()
